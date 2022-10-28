594192e489a47752869756034be5a762c63ec5ab
KJEKJE <55700917+KJEKJE@users.noreply.github.com>
2022-10-29 00:37:03 +0100

    Invented Experimental Movement Script
    A movement script for controlling a character has been created (specifically on the KjeBranch) as a fun experiment - the likelihood is that Chris' player movement will be used, but this serves as a refresher for the other team members, who have not coded in a long time.

--- Impending Bacon/Assets/scripts/Movement.cs
+++ Impending Bacon/Assets/scripts/Movement.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public Rigidbody rB;
-    public float fForce = 200f;
+    public float fForce = 200f; //forward+back force
+    public float sForce = 300f; //left+right force
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,26 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        rB.AddForce(0, 0, fForce * Time.deltaTime);
+        //+fForce
+        if (Input.GetKey("d"))
+        {
+            rB.AddForce(sForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rB.AddForce(-sForce * Time.deltaTime, 0, 0);
+        }
+        
+        if (Input.GetKey("w"))
+        {
+            rB.AddForce(0, 0, fForce * Time.deltaTime);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            rB.AddForce(0, 0, -fForce * Time.deltaTime);
+        }
+
     }
 }
